32a4d4e28d98662e964a93d9dc39bbed19d74162
pacoteémato <joaovitor250p@gmail.com>
2026-08-30 21:15:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,22 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'pacote-e-mato-v5';
+
+self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
